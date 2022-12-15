475ddce6fa7e0e4cd73a7d7517946b0a679026e0
BurrL <119257172+BurrL@users.noreply.github.com>
2022-12-15 23:04:35 +0200

    Add files via upload

---
+++ jj.js
@@ -0,0 +1,21 @@
+console.log("all")
+console.log("YAY!! THE PAGE IS RUN")
+
+alert("HEY THERE")
+
+document.getElementById("info").innerHTML ="Snorlax , :"+ "sleeping pokemon "  ;
+
+var ep1 = 5;
+var ep2 = 10;
+var ep3 = 12;
+var ep4 = 24;
+var ep5 = 27;
+var ep6 = 51;
+var ep7 = 60;
+var ep8 = 100;
+var ep9 = 105;
+var ep10 = 101;
+
+var result = (ep1 + ep2 + ep3 + ep4 + ep5 + ep6 + ep7 + ep8 + ep9 + ep10)/10
+
+document.getElementById("averge").innerHTML ="THE AVERGE OF SNORLA'X APPEARAUNCE IN THE SHOW IS :"+ "" + result ;
